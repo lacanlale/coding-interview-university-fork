e8d7b85118a5981615464683b9f4835c9e64b5f4
Jonathan Lacanlale <jonathalacanlale98@gmail.com>
2023-11-06 18:28:45 -0800

    Adding skeleton code

---
+++ progress/data_structures/linked_lists.py
@@ -0,0 +1,49 @@
+class Singly():
+    def __init__(self, value):
+        self.value = value
+        self.tail = None
+
+    def __str__(self):
+        pass
+    
+    def __len__(self):
+        pass
+
+    def empty(self):
+        pass
+    
+    def value_at(self, index):
+        pass
+
+    def pop_front(self):
+        pass
+    
+    def push_back(self, value):
+        pass
+
+    def front(self):
+        pass
+
+    def back(self):
+        pass
+
+    def insert(self, index, value):
+        pass
+
+    def erase(self, index):
+        pass
+
+    def value_n_from_end(self, n):
+        pass
+
+    def reverse(self):
+        pass
+
+    def remove_value(self, value):
+        pass
+
+class Doubly():
+    def __init__(self, value, head):
+        self.head = head
+        self.value = value
+        self.tail = None
